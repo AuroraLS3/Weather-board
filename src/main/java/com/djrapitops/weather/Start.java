c12aa66d51ce625743d7a3f5106bbb53508e7ec7
Rsl1122 <24460436+Rsl1122@users.noreply.github.com>
2020-02-15 11:11:58 +0200

    Fix start

--- src/main/java/com/djrapitops/weather/Start.java
+++ src/main/java/com/djrapitops/weather/Start.java
@@ -2,6 +2,8 @@ package com.djrapitops.weather;
 
 import akka.actor.typed.ActorSystem;
 
+import java.util.Scanner;
+
 /**
  * Main class.
  *
@@ -10,11 +12,29 @@ import akka.actor.typed.ActorSystem;
 public class Start {
 
     public static void main(String[] args) {
-        ActorSystem<Bootstrapper.Initialize> system = ActorSystem.create(Bootstrapper.create(), "Process Bootstrapper");
+        ActorSystem<Bootstrapper.Initialize> system = ActorSystem.create(Bootstrapper.create(), "Bootstrapper");
 
         system.tell(new Bootstrapper.Initialize());
 
+        runReadCommandLoop();
+
         system.terminate();
+    }
+
+    private static void runReadCommandLoop() {
+        try (Scanner in = new Scanner(System.in)) {
+            while (true) {
+                if (in.hasNextLine()) {
+                    String command = in.nextLine();
+                    switch (command.toLowerCase().trim()) {
+                        case "stop": return;
+                        case "quit": return;
+                        case "exit": return;
+                        default: break;
+                    }
+                }
+            }
+        }
     }
 
 }
